48dc54e31af65f3949a7c93c543e024a81faa1b3
BLUEMOON233 <liuwenyue233@gmail.com>
2023-05-15 10:07:22 +0800

    2023_05_15

---
+++ competition/cpp/codeforces/Codeforces_Round_873_div2/c.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+typedef unsigned int UI;
+typedef pair<int, int> PII;
+#define endl '\n'
+#define rep(i,j,k) for(int i=int(j);i<=int(k);i++)
+#define per(i,j,k) for(int i=int(j);i>=int(k);i--)
+#define mes(tmp, data) memset(tmp, data, sizeof tmp)
+#define debug(x) cerr << "(" << __LINE__ << ")" << #x << " = " << x << endl;
+#define debug0(tmp, Size) rep(i, 0, Size - 1) cout<<tmp[i]<<" \n"[i == Size - 1]
+#define debug1(tmp, Size) rep(i, 1, Size) cout<<tmp[i]<<" \n"[i == Size]
+#define fast() ios::sync_with_stdio(false); cin.tie(nullptr)
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define Yes cout << "Yes\n"
+#define No cout << "No\n"
+template<class T>
+inline T read() {
+	T x = 0, f = 1;
+	char ch = getchar();
+	while (!isdigit(ch)) {
+		if (ch == '-') f = -1;
+		ch = getchar();
+	}
+	while (isdigit(ch)) {
+		x = (x << 1) + (x << 3) + (ch ^ 48);
+		ch = getchar();
+	}
+	return x * f;
+}
+template<class T>
+inline void write(T x) {
+	if (x < 0) putchar('-'), x = -x;
+	if (x > 9) write(x / 10);
+	putchar(x % 10 + '0');
+	return;
+}
+#define read() read<int>()
+#define write(tmp) write<int>(tmp);
+//#define read() read<LL>()
+//#define write(tmp) write<LL>(tmp);
+//#define read() read<__int128>()
+//#define write(tmp) write<__int128>(tmp);
+
+const int N = 1, mod = 1e9 + 7;
+
+inline void solve() {
+	int n;
+	cin >> n;
+	vector<LL> a(n + 1, 0), b(n + 1, 0);
+	rep(i, 1, n) cin >> a[i];
+	rep(i, 1, n) cin >> b[i];
+	sort(a.begin() + 1, a.end());
+	sort(b.begin() + 1, b.end());
+	LL rs = 1;
+	rep(i, 1, n) {
+		int pos = lower_bound(b.begin() + 1, b.end(), a[i]) - b.begin();
+		if (pos - i <= 0) {
+			cout << 0 << '\n';
+			return;
+		}
+		rs = rs * (pos - i) % mod;
+	}
+	cout << rs << '\n';
+}
+
+int main() {
+	fast();
+	int T = 1;
+	//	T = read();
+	cin >> T;
+	while (T--) solve();
+}
