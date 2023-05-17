cb1de5bdc4e59403db6bcfe392901bda4152abb7
BLUEMOON233 <liuwenyue233@gmail.com>
2023-05-17 10:26:05 +0800

    2023_05_17

--- competition/cpp/temp/1.cpp
+++ competition/cpp/temp/1.cpp
@@ -43,71 +43,10 @@ inline void write(T x) {
 //#define read() read<__int128>()
 //#define write(tmp) write<__int128>(tmp);
 
-const int N = 1e6 + 7;
-
-LL w[N], n, m;
-struct Node {
-	int l, r;
-	LL sum, add;
-} tr[N << 2];
-void pushup(int u) {
-	tr[u].sum = tr[u << 1].sum + tr[u << 1 | 1].sum;
-}
-void pushdown(int u) {
-	Node &rt = tr[u], &lp = tr[u << 1], &rp = tr[u << 1 | 1];
-	if (rt.add) {
-		lp.add += rt.add, lp.sum += (LL)(lp.r - lp.l + 1) * rt.add;
-		rp.add += rt.add, rp.sum += (LL)(rp.r - rp.l + 1) * rt.add;
-		rt.add = 0;
-	}
-}
-void build(int u, int l, int r) {
-	if (l == r) tr[u] = {l, r, w[l], 0};
-	else {
-		tr[u] = {l, r};
-		int mid = l + r >> 1;
-		build(u << 1, l, mid), build(u << 1 | 1, mid + 1, r);
-		pushup(u);
-	}
-}
-void modify(int u, int l, int r, LL v) {
-	if (l <= tr[u].l && tr[u].r <= r) {
-		tr[u].sum += (LL)(tr[u].r - tr[u].l + 1) * v;
-		tr[u].add += v;
-	} else {
-		pushdown(u);
-		int mid = tr[u].l + tr[u].r >> 1;
-		if (l <= mid) modify(u << 1, l, r, v);
-		if (r > mid) modify(u << 1 | 1, l, r, v);
-		pushup(u);
-	}
-}
-LL query(int u, int l, int r) {
-	if (l <= tr[u].l && tr[u].r <= r) return tr[u].sum;
-	pushdown(u);
-	int mid = tr[u].l + tr[u].r >> 1;
-	LL rs = 0;
-	if (l <= mid) rs = query(u << 1, l, r);
-	if (r > mid) rs += query(u << 1 | 1, l, r);
-	return rs;
-}
+const int N = 1;
 
 inline void solve() {
-	int n, q;
-	cin >> n >> q;
-	rep(i, 1, n) cin >> w[i];
-	build(1, 1, n);
-	while (q--) {
-		int opt, l, r, x;
-		cin >> opt;
-		if (opt == 1) {
-			cin >> l >> r >> x;
-			modify(1, l, r, x);
-		} else {
-			cin >> l >> r;
-			cout << query(1, l, r) << '\n';
-		}
-	}
+
 }
 
 int main() {
