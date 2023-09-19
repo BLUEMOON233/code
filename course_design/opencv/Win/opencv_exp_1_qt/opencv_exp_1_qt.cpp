1f333bf29dd8579bc541b3caecdb87043b39c687
BLUEMOON233 <liuwenyue233@gmail.com>
2023-09-19 19:26:40 +0800

    2023_09_19

--- course_design/opencv/Win/opencv_exp_1_qt/opencv_exp_1_qt.cpp
+++ course_design/opencv/Win/opencv_exp_1_qt/opencv_exp_1_qt.cpp
@@ -4,13 +4,15 @@ opencv_exp_1_qt::opencv_exp_1_qt(QWidget* parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+	ui.brightness_slider->setRange(0, 100);
+	ui.blur_slider->setRange(1, 20);//设置滑动条的范围
 
-	ui.brightnessSlider->setRange(0, 100);
+	cv::Mat ori_image = cv::imread("./lena.jpg"); //读取图片
+	cv::Mat image; //副本数据 
+	double brightness_factor = 1; //亮度因子
+	int kernel_size = 1; //滤波器大小
 
-	cv::Mat ori_image = cv::imread("./lena.jpg");
-	cv::Mat image;
-	double brightness_factor = 0;
-	//init:
+	//初始化操作：
 	cv::cvtColor(ori_image, image, cv::COLOR_BGR2RGB);
 	image.convertTo(image, -1, brightness_factor, 0);
 	QImage qImage(image.data, image.cols, image.rows, image.cols * image.channels(), QImage::Format_RGB888);
@@ -18,16 +20,27 @@ opencv_exp_1_qt::opencv_exp_1_qt(QWidget* parent)
 	ui.image_label->setPixmap(pixmap);
 
 	if (!ori_image.empty()) {
-		QObject::connect(ui.brightnessSlider, &QSlider::valueChanged, [=](int value)mutable {
-			//ui.image_label->setText(QString::number(value));
-			brightness_factor = double(value) / 20;
-			cv::cvtColor(ori_image, image, cv::COLOR_BGR2RGB);
-			image.convertTo(image, -1, brightness_factor, 0);
-			QImage qImage(image.data, image.cols, image.rows, image.cols * image.channels(), QImage::Format_RGB888);
-			QPixmap pixmap = QPixmap::fromImage(qImage.scaled(ui.image_label->size(), Qt::KeepAspectRatio));
-			ui.image_label->setPixmap(pixmap);
+		QObject::connect(ui.brightness_slider, &QSlider::valueChanged, [=, &brightness_factor, &kernel_size](int value)mutable {
+			brightness_factor = double(value) / 20;//根据滑动条计算亮度因子
+			cv::cvtColor(ori_image, image, cv::COLOR_BGR2RGB);//将原始图片的BGR格式转化为RGB格式
+			image.convertTo(image, -1, brightness_factor, 0);//修改图片的亮度
+			cv::blur(image, image, cv::Size(kernel_size, kernel_size));//模糊操作
+			QImage qImage(image.data, image.cols, image.rows, image.cols * image.channels(), QImage::Format_RGB888);//将opencv格式转化为Qimage格式
+			QPixmap pixmap = QPixmap::fromImage(qImage.scaled(ui.image_label->size(), Qt::KeepAspectRatio));//调整大小
+			ui.image_label->setPixmap(pixmap);//将图片放入Qlabel控件中
+			});
+
+		QObject::connect(ui.blur_slider, &QSlider::valueChanged, [=, &brightness_factor, &kernel_size](int value)mutable {
+			kernel_size = value;//根据滑动条修改滤波器的大小
+			cv::cvtColor(ori_image, image, cv::COLOR_BGR2RGB);//将原始图片的BGR格式转化为RGB格式
+			image.convertTo(image, -1, brightness_factor, 0);//修改图片的亮度
+			cv::blur(image, image, cv::Size(kernel_size, kernel_size));//模糊操作
+			QImage qImage(image.data, image.cols, image.rows, image.cols * image.channels(), QImage::Format_RGB888);//将opencv格式转化为Qimage格式
+			QPixmap pixmap = QPixmap::fromImage(qImage.scaled(ui.image_label->size(), Qt::KeepAspectRatio));//调整大小
+			ui.image_label->setPixmap(pixmap);//将图片放入Qlabel控件中
 			});
 	}
+
 }
 
 opencv_exp_1_qt::~opencv_exp_1_qt()
--- course_design/opencv/Win/opencv_exp_1_qt/x64/Debug/uic/ui_opencv_exp_1_qt.h
+++ course_design/opencv/Win/opencv_exp_1_qt/x64/Debug/uic/ui_opencv_exp_1_qt.h
@@ -11,6 +11,7 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QGroupBox>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
@@ -26,7 +27,10 @@ class Ui_opencv_exp_1_qtClass
 public:
     QWidget *centralWidget;
     QLabel *image_label;
-    QSlider *brightnessSlider;
+    QGroupBox *groupBox;
+    QSlider *brightness_slider;
+    QGroupBox *groupBox_2;
+    QSlider *blur_slider;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
     QStatusBar *statusBar;
@@ -40,11 +44,23 @@ public:
         centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
         image_label = new QLabel(centralWidget);
         image_label->setObjectName(QString::fromUtf8("image_label"));
-        image_label->setGeometry(QRect(10, 10, 361, 321));
-        brightnessSlider = new QSlider(centralWidget);
-        brightnessSlider->setObjectName(QString::fromUtf8("brightnessSlider"));
-        brightnessSlider->setGeometry(QRect(430, 40, 160, 22));
-        brightnessSlider->setOrientation(Qt::Horizontal);
+        image_label->setGeometry(QRect(30, 10, 301, 321));
+        image_label->setFrameShape(QFrame::Box);
+        image_label->setLineWidth(3);
+        groupBox = new QGroupBox(centralWidget);
+        groupBox->setObjectName(QString::fromUtf8("groupBox"));
+        groupBox->setGeometry(QRect(370, 10, 191, 51));
+        brightness_slider = new QSlider(groupBox);
+        brightness_slider->setObjectName(QString::fromUtf8("brightness_slider"));
+        brightness_slider->setGeometry(QRect(10, 20, 171, 22));
+        brightness_slider->setOrientation(Qt::Horizontal);
+        groupBox_2 = new QGroupBox(centralWidget);
+        groupBox_2->setObjectName(QString::fromUtf8("groupBox_2"));
+        groupBox_2->setGeometry(QRect(370, 70, 191, 51));
+        blur_slider = new QSlider(groupBox_2);
+        blur_slider->setObjectName(QString::fromUtf8("blur_slider"));
+        blur_slider->setGeometry(QRect(10, 20, 171, 22));
+        blur_slider->setOrientation(Qt::Horizontal);
         opencv_exp_1_qtClass->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(opencv_exp_1_qtClass);
         menuBar->setObjectName(QString::fromUtf8("menuBar"));
@@ -64,8 +80,10 @@ public:
 
     void retranslateUi(QMainWindow *opencv_exp_1_qtClass)
     {
-        opencv_exp_1_qtClass->setWindowTitle(QCoreApplication::translate("opencv_exp_1_qtClass", "opencv_exp_1_qt", nullptr));
+        opencv_exp_1_qtClass->setWindowTitle(QCoreApplication::translate("opencv_exp_1_qtClass", "OPENCV-\347\224\250\346\210\267\344\272\244\344\272\222\347\252\227\345\217\243(with QT)", nullptr));
         image_label->setText(QString());
+        groupBox->setTitle(QCoreApplication::translate("opencv_exp_1_qtClass", "\344\272\256\345\272\246", nullptr));
+        groupBox_2->setTitle(QCoreApplication::translate("opencv_exp_1_qtClass", "\346\250\241\347\263\212", nullptr));
     } // retranslateUi
 
 };
